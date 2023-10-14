cb22d8dae20a7cf4267e8ecd30a74b01b02d4a1d
taejong12 <dbxowhdsla12@naver.com>
2023-10-14 19:16:34 +0900

    초기 설정

---
+++ front/src/test/java/com/ysbc/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysbc.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
